a289d24f6a63aad18b0322aaeb45bf8d76c9ac0b
ImranHossain00 <imran188566@gmail.com>
2023-10-11 03:53:15 +0600

    First Commit

---
+++ src/main/java/com/example/amicodingparina/AmiCodingParinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.amicodingparina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiCodingParinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiCodingParinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amicodingparina/AmiCodingParinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amicodingparina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiCodingParinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
